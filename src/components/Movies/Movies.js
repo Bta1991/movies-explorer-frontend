3664ad27c2b400c6e40a5257dbf9409ce719f06d
tanyaka <litium@yandex.ru>
2023-10-29 19:10:56 +0300

    navbar исправлена label-aria, добавлен css для блока фильмы

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,3 +1,4 @@
+import './Movies.css'
 import React, { useState, useEffect, useCallback } from 'react'
 import MoviesCardList from './MoviesCardList/MoviesCardList'
 import SearchForm from './SearchForm/SearchForm'
